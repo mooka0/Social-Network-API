8fe60296da716abb6ca4b01a75aa0517815bb647
mooka0 <angelankao@gmail.com>
2021-04-12 22:29:30 -0700

    routes set up

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useFindAndModify: false,
   useNewUrlParser: true,
