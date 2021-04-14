7d4f1a7a18961592c57c8e3136431e19d0d48fa1
mooka0 <angelankao@gmail.com>
2021-04-13 19:34:22 -0700

    working code

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+
+const mongoose = require('mongoose');
 const express = require('express');
-const mongoose = require('mongoose')
+
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -7,11 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
